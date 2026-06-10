1cb350899b1aaab7f6445d0180f9f878b02d0a33
Rapptz <rapptz@gmail.com>
2026-06-10 15:35:44 -0400

    Use normalized discord.py package name in ?about

--- cogs/stats.py
+++ cogs/stats.py
@@ -295,7 +295,7 @@ class Stats(commands.Cog):
         cpu_usage = self.process.cpu_percent() / psutil.cpu_count()
         embed.add_field(name='Process', value=f'{memory_usage:.2f} MiB\n{cpu_usage:.2f}% CPU')
 
-        version = pkg_resources.get_distribution('discord.py').version
+        version = pkg_resources.get_distribution('discord-py').version
         embed.add_field(name='Guilds', value=guilds)
         embed.add_field(name='Commands Run', value=sum(self.bot.command_stats.values()))
         embed.add_field(name='Uptime', value=self.get_bot_uptime(brief=True))
